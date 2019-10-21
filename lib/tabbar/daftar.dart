f443cb71b4697d8c6ca5e50b629d1e279a6a0ad3
mohali9 <mohali.ilkom@gmail.com>
2019-10-21 11:07:42 +0700

    tambahan

--- lib/tabbar/daftar.dart
+++ lib/tabbar/daftar.dart
@@ -1,11 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
+import 'dart:async';
+import 'package:intl/intl.dart';
+import 'package:datetime_picker_formfield/datetime_picker_formfield.dart';
 
 class Daftar extends StatefulWidget {
   @override
-  _DaftarState createState() => _DaftarState();
+  _DaftarState createState() => new _DaftarState();
 }
 
 class _DaftarState extends State<Daftar> {
+  // Tanggal
+  DateTime _date = DateTime.now();
+  // TimeOfDay _time = TimeOfDay.now();
+
+  Future<Null> _selectDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+      context: context,
+      initialDate: _date,
+      firstDate: DateTime(2016),
+      lastDate: DateTime(2019),
+    );
+    if (picked != null && picked != _date) {
+      print('Date Selected : ${_date.toString()}');
+      setState(() {
+        _date = picked;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -53,6 +76,33 @@ class _DaftarState extends State<Daftar> {
             title: const Text('Contact group'),
             subtitle: const Text('Not specified'),
           ),
+          new FlatButton(
+            onPressed: () {
+              DatePicker.showDatePicker(context,
+                  showTitleActions: true,
+                  minTime: DateTime(1990),
+                  maxTime: DateTime(2019), onChanged: (date) {
+                print('change $date');
+              }, onConfirm: (date) {
+                print('confirm $date');
+              }, currentTime: DateTime.now(), locale: LocaleType.id);
+            },
+            child: Text(
+              'Birthday',
+              style: TextStyle(color: Colors.blue),
+            ),
+          ),
+          new Column(
+            children: <Widget>[
+              new Text('Date Selected : ${_date.toString()}'),
+              new RaisedButton(
+                child: Text('Select Date'),
+                onPressed: () {
+                  _selectDate(context);
+                },
+              ),
+            ],
+          ),
           new Row(
             children: <Widget>[
               new RaisedButton(
@@ -68,3 +118,78 @@ class _DaftarState extends State<Daftar> {
     );
   }
 }
+
+// class CustomPicker extends CommonPickerModel {
+//   String digits(int value, int length) {
+//     return '$value'.padLeft(length, "0");
+//   }
+
+//   CustomPicker({DateTime currentTime, LocaleType locale})
+//       : super(locale: locale) {
+//     this.currentTime = currentTime ?? DateTime.now();
+//     this.setLeftIndex(this.currentTime.hour);
+//     this.setMiddleIndex(this.currentTime.minute);
+//     this.setRightIndex(this.currentTime.second);
+//   }
+
+//   @override
+//   String leftStringAtIndex(int index) {
+//     if (index >= 0 && index < 24) {
+//       return this.digits(index, 2);
+//     } else {
+//       return null;
+//     }
+//   }
+
+//   @override
+//   String middleStringAtIndex(int index) {
+//     if (index >= 0 && index < 60) {
+//       return this.digits(index, 2);
+//     } else {
+//       return null;
+//     }
+//   }
+
+//   @override
+//   String rightStringAtIndex(int index) {
+//     if (index >= 0 && index < 60) {
+//       return this.digits(index, 2);
+//     } else {
+//       return null;
+//     }
+//   }
+
+//   @override
+//   String leftDivider() {
+//     return "|";
+//   }
+
+//   @override
+//   String rightDivider() {
+//     return "|";
+//   }
+
+//   @override
+//   List<int> layoutProportions() {
+//     return [1, 2, 1];
+//   }
+
+//   @override
+//   DateTime finalTime() {
+//     return currentTime.isUtc
+//         ? DateTime.utc(
+//             currentTime.year,
+//             currentTime.month,
+//             currentTime.day,
+//             this.currentLeftIndex(),
+//             this.currentMiddleIndex(),
+//             this.currentRightIndex())
+//         : DateTime(
+//             currentTime.year,
+//             currentTime.month,
+//             currentTime.day,
+//             this.currentLeftIndex(),
+//             this.currentMiddleIndex(),
+//             this.currentRightIndex());
+//   }
+// }
